1f9177cdcd2a9536e73af2ac24800c729f7d00e5
Mahdi Bahmanpour <Mahdi.bahmanpour97@gmail.com>
2021-06-10 16:04:43 +0430

    Create WelcomeActivity.kt and add neo.json to project

--- app/src/main/java/com/mahdiba97/neo/WelcomeActivity.kt
+++ app/src/main/java/com/mahdiba97/neo/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package com.mahdiba97.neo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
         supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 5500)
     }
 }
