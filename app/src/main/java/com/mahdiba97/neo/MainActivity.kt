3a0cb164a67d0dedd831de19f130d6ec25203128
Mahdi Bahmanpour <Mahdi.bahmanpour97@gmail.com>
2021-06-10 14:06:29 +0430

    Initial commit

---
+++ app/src/main/java/com/mahdiba97/neo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mahdiba97.neo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/mahdiba97/neo/WelcomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.mahdiba97.neo
+
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
