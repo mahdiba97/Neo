ac5f8c7660b6ea947908463c5756105c31ad25a7
Mahdi Bahmanpour <Mahdi.bahmanpour97@gmail.com>
2021-06-11 18:01:58 +0430

    Specify entity for room

--- app/src/main/java/com/mahdiba97/neo/data/NoteEntity.kt
+++ app/src/main/java/com/mahdiba97/neo/data/NoteEntity.kt
@@ -1,9 +1,18 @@
 package com.mahdiba97.neo.data
 
-data class NoteEntity(
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.mahdiba97.neo.NEW_NOTE_ID
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "notes")
+class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
-    val priority: Int,
-    val text: String
-) {
-    constructor(priority: Int, text: String) : this(0, priority, text)
+    var priority: Int,
+    var text: String
+) : Parcelable {
+    constructor(priority: Int, text: String) : this(NEW_NOTE_ID, priority, text)
 }
