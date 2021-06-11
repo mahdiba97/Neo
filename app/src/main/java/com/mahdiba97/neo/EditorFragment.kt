43a36617d0fbf5bf10e73ff1585b091958d2cba8
Mahdi Bahmanpour <Mahdi.bahmanpour97@gmail.com>
2021-06-11 17:52:26 +0430

    Add check icon to EditorFragment.kt

--- app/src/main/java/com/mahdiba97/neo/EditorFragment.kt
+++ app/src/main/java/com/mahdiba97/neo/EditorFragment.kt
@@ -2,10 +2,14 @@ package com.mahdiba97.neo
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.mahdiba97.neo.databinding.EditorFragmentBinding
 
@@ -16,11 +20,41 @@ class EditorFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_check)
+        }
+        setHasOptionsMenu(true)
         binding = EditorFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(EditorViewModel::class.java)
         binding.noteEditor.setText(args.id.toString())
+
+        // When user press back button saveAndReturn method will revoke
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    saveAndReturn()
+                }
+            })
+
         return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> saveAndReturn()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun saveAndReturn(): Boolean {
+        findNavController().navigateUp()
+        return true
     }
 
 }
--- app/src/main/java/com/mahdiba97/neo/MainFragment.kt
+++ app/src/main/java/com/mahdiba97/neo/MainFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -22,6 +23,9 @@ class MainFragment : Fragment(), NotesListAdapter.ListItemListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(false)
+        }
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding = MainFragmentBinding.inflate(inflater, container, false)
         with(binding.mainRecycler) {
