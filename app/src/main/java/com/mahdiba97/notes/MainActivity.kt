0b912341943348bf49f47953700074f40c8b63e5
Mahdi Bahmanpour <Mahdi.bahmanpour97@gmail.com>
2021-09-02 17:23:53 +0430

    Add intent filter to get text from other apps

--- app/src/main/java/com/mahdiba97/notes/MainActivity.kt
+++ app/src/main/java/com/mahdiba97/notes/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.mahdiba97.notes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.mahdiba97.notes.data.NoteEntity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: EditorViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         PrefHelper.setLanguage(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val received = intent.getStringExtra(Intent.EXTRA_TEXT)
+        if (received != null) {
+            viewModel = ViewModelProvider(this)[EditorViewModel::class.java]
+            viewModel.insertNote(NoteEntity(received))
+        }
     }
 
 
