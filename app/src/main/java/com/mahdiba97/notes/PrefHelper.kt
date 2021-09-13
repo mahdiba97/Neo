a18bfc3eb3b1044e804943e203ee9f1f04a7def2
Mahdi Bahmanpour <mahdi.bahmanpour97@gmail.com>
2021-09-14 02:38:09 +0430

    Add new methods to change theme and language

--- app/src/main/java/com/mahdiba97/notes/PrefHelper.kt
+++ app/src/main/java/com/mahdiba97/notes/PrefHelper.kt
@@ -8,33 +8,45 @@ import java.util.*
 
 
 class PrefHelper {
-    companion object {
-        fun pref(context: Context): SharedPreferences =
-            PreferenceManager.getDefaultSharedPreferences(context)
+  companion object {
+    private fun pref(context: Context): SharedPreferences =
+      PreferenceManager.getDefaultSharedPreferences(context)
 
 
-        fun setTheme(context: Context) {
-            when (pref(context).getString(THEME_KEY, "Default").toString()) {
-                "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                "Night" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            }
-        }
-
-        fun setLanguage(context: Context) {
-
-            val local = when (pref(context).getString(LANGUAGE_KEY, "Default").toString()) {
-                "Persian" -> Locale("fa")
-                else -> Locale("en")
-            }
-            val resources = context.resources
-            Locale.setDefault(local)
-            resources.configuration.setLocale(local)
-            resources.updateConfiguration(resources.configuration, resources.displayMetrics)
-        }
-
-        fun getLanguage(context: Context): String {
-            return pref(context).getString(LANGUAGE_KEY, "English").toString()
-        }
+    fun setTheme(context: Context) {
+      when (pref(context).getString(THEME_KEY, "Default").toString()) {
+        "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        "Night" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+      }
     }
+
+    fun setTheme(key: String) {
+      when (key) {
+        "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        "Night" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+      }
+    }
+
+    fun setLanguage(context: Context) {
+
+      val local = when (pref(context).getString(LANGUAGE_KEY, "Default").toString()) {
+        "Persian" -> Locale("fa")
+        else -> Locale("en")
+      }
+      val resources = context.resources
+      Locale.setDefault(local)
+      resources.configuration.setLocale(local)
+      resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+    }
+
+    fun getLanguage(context: Context): String {
+      return pref(context).getString(LANGUAGE_KEY, "English").toString()
+    }
+
+    fun getTheme(context: Context): String {
+      return pref(context).getString(THEME_KEY, "Default").toString()
+    }
+  }
 }
--- app/src/main/java/com/mahdiba97/notes/SettingsActivity.kt
+++ app/src/main/java/com/mahdiba97/notes/SettingsActivity.kt
@@ -4,46 +4,52 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
-        title = getString(R.string.settings)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.settings_activity)
+    title = getString(R.string.settings)
+    if (savedInstanceState == null) {
+      supportFragmentManager
+        .beginTransaction()
+        .replace(R.id.settings, SettingsFragment())
+        .commit()
     }
 
-    override fun onStart() {
-        super.onStart()
-        PrefHelper.pref(this).registerOnSharedPreferenceChangeListener { _, key ->
-            if (key == THEME_KEY) PrefHelper.setTheme(this)
-            val myIntent = Intent(this, MainActivity::class.java)
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(myIntent)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == android.R.id.home) {
+      onBackPressed()
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+      setPreferencesFromResource(R.xml.root_preferences, rootKey)
+      findPreference<ListPreference>("language_key")?.setOnPreferenceChangeListener { preference, newValue ->
+        if (newValue.toString() != PrefHelper.getLanguage(requireContext())) {
+          val myIntent = Intent(requireContext(), MainActivity::class.java)
+          myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+          myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+          startActivity(myIntent)
         }
+        true
+      }
+
+      findPreference<ListPreference>("theme_key")?.setOnPreferenceChangeListener { preference, newValue ->
+        if (newValue != PrefHelper.getTheme(requireContext()))
+          PrefHelper.setTheme(newValue.toString())
+
+        true
+      }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            onBackPressed()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
-    }
+  }
 
 }
